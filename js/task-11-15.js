582a015408ac6fc985af91aa95e7704fe2930ee4
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-03-13 18:48:26 +0200

    resolving all tasks

--- js/task-11-15.js
+++ js/task-11-15.js
@@ -1,14 +1,78 @@
-// Створити змінні для зберігання цілочисельного значення та рядка з числом у вигляді рядка. Використати метод parseInt() для перетворення рядка у ціле число та вивести результат в консоль. Потім використати метод toString() для перетворення цілочисельного значення у рядок та вивести результат в консоль.
+// 11. Створити змінні для зберігання цілочисельного значення та рядка з числом у вигляді рядка. Використати метод parseInt() для перетворення рядка у ціле число та вивести результат в консоль. Потім використати метод toString() для перетворення цілочисельного значення у рядок та вивести результат в консоль.
 const intNum = 15;
 const strNum = '15px';
-console.log(Number.parseInt(strNum));
-console.log(intNum.toString());
+// console.log(Number.parseInt(strNum));
+// console.log(intNum.toString());
 
-// Створити розмітку  з текстовим полем та кнопкою. При натисканні на кнопку перевіряти, чи введений текст містить слово "JavaScript", якщо так, то виводити повідомлення "Текст містить слово JavaScript", якщо ні - "Текст не містить слово JavaScript".
+// 12. Створити розмітку  з текстовим полем та кнопкою. При натисканні на кнопку перевіряти, чи введений текст містить слово "JavaScript", якщо так, то виводити повідомлення "Текст містить слово JavaScript", якщо ні - "Текст не містить слово JavaScript".
 
+function checkUserInput() {
+  const userText = document.getElementById('user-input').value;
+  const paragrInfo = document.getElementById('res');
+  if (userText.toLowerCase().includes('javascript')) {
+    paragrInfo.innerText = 'Текст містить слово JavaScript';
+  } else {
+    paragrInfo.innerText = 'Текст не містить слово JavaScript';
+  }
+}
+// 13. Створити розмітку  з формою з полями введення та кнопкою. При натисканні на кнопку перевіряти, чи введене число більше за 10 та менше за 20, якщо так, то виводити повідомлення "Число входить в діапазон від 10 до 20", якщо ні - "Число не входить в діапазон від 10 до 20".
 
-// Створити розмітку  з формою з полями введення та кнопкою. При натисканні на кнопку перевіряти, чи введене число більше за 10 та менше за 20, якщо так, то виводити повідомлення "Число входить в діапазон від 10 до 20", якщо ні - "Число не входить в діапазон від 10 до 20".
+function checkNumInput() {
+  const numInput = Number(document.getElementById('num-input').value);
+  const res = document.getElementById('num-check-res');
+  res.innerText =
+    numInput > 10 && numInput < 20
+      ? `Число ${numInput} входить в діапазон від 10 до 20`
+      : `Число ${numInput} не входить в діапазон від 10 до 20`;
+  document.getElementById('num-input').value = '';
+}
 
-// Створити розмітку  з формою з полями введення та кнопкою. При натисканні на кнопку перевіряти, чи введені дані правильно заповнені, якщо так, то перенаправляти на іншу сторінку, якщо ні - виводити повідомлення про помилку. Правильно заповнені дані: ім'я містить не менше 3 символів, email містить символ @ та крапку після неї, пароль містить не менше 6 символів.
+// 14. Створити розмітку  з формою з полями введення та кнопкою. При натисканні на кнопку перевіряти, чи введені дані правильно заповнені, якщо так, то перенаправляти на іншу сторінку, якщо ні - виводити повідомлення про помилку. Правильно заповнені дані: ім'я містить не менше 3 символів, email містить символ @ та крапку після неї, пароль містить не менше 6 символів.
 
-// Створити розмітку  з полем введення, що приймає рядок та кнопкою. При натисканні на кнопку перевіряти, чи введений рядок є днем тижня (наприклад: "понеділок", "вівторок" і т.д.), виводити відповідне повідомлення про робочий день або вихідний. Використовувати switch.
+function checkUserInfo() {
+  const userName = document.getElementById('user-name-input').value;
+  const userEmail = document.getElementById('user-email-input').value;
+  const userPassword = document.getElementById('user-password-input').value;
+
+  const res = document.getElementById('check-info-res');
+  res.innerText =
+    checkName(userName) && checkEmail(userEmail) && checkPassword(userPassword)
+      ? 'Check is done!'
+      : 'Check is failed';
+  document.getElementById('user-name-input').value = '';
+  document.getElementById('user-email-input').value = '';
+  document.getElementById('user-password-input').value = '';
+}
+function checkName(name) {
+  return name.length > 3;
+}
+function checkEmail(email) {
+  return email.includes('@.') && email[email.length - 1] !== '.';
+}
+function checkPassword(password) {
+  return password.length >= 6;
+}
+// 15. Створити розмітку  з полем введення, що приймає рядок та кнопкою. При натисканні на кнопку перевіряти, чи введений рядок є днем тижня (наприклад: "понеділок", "вівторок" і т.д.), виводити відповідне повідомлення про робочий день або вихідний. Використовувати switch.
+
+function checkDayOfWeek() {
+  const inputDay = document
+    .getElementById('user-day-input')
+    .value.toLowerCase();
+  const res = document.getElementById('check-day-res');
+  switch (inputDay) {
+    case 'понеділок':
+    case 'вівторок':
+    case 'середа':
+    case 'четвер':
+    case "п'ятниця":
+      res.innerText = `${inputDay} - робочий день`;
+      break;
+    case 'субота':
+    case 'неділя':
+      res.innerText = `${inputDay} - вихідний день`;
+      break;
+    default:
+      res.innerText = 'Неправильно введена назва дня';
+  }
+  document.getElementById('user-day-input').value = '';
+}
--- js/task-16-21.js
+++ js/task-16-21.js
@@ -1,29 +1,159 @@
-// Створити розмітку  з полем введення, що приймає номер місяця та кнопкою. При натисканні на кнопку виводити повідомлення про пору року, до якої належить цей місяць.Використовувати switch.
+// 16. Створити розмітку  з полем введення, що приймає номер місяця та кнопкою. При натисканні на кнопку виводити повідомлення про пору року, до якої належить цей місяць.Використовувати switch.
 
-// Створити розмітку  з полем введення, що приймає номер місяця та кнопкою. При натисканні на кнопку виводити кількість днів у цьому місяці.Використовувати switch.
+function getSeason() {
+  const monthOrder = Number(document.getElementById('month-input').value);
+  const res = document.getElementById('season-res');
 
-// Створити розмітку  з полем введення, що приймає назву кольору (наприклад, "червоний", "синій" і т.д.) та кнопкою. При натисканні на кнопку виводити відповідне повідомлення про дію: якщо це "червоний" - "стоп", якщо "зелений" - "йти", якщо "жовтий" - "чекати". Використовувати switch.
+  switch (monthOrder) {
+    case 12:
+    case 1:
+    case 2:
+      res.innerText = `${monthOrder} month it's Winter`;
+      break;
+    case 3:
+    case 4:
+    case 5:
+      res.innerText = `${monthOrder} month it's Spring`;
+      break;
+    case 6:
+    case 7:
+    case 8:
+      res.innerText = `${monthOrder} month it's Summer`;
+      break;
+    case 9:
+    case 10:
+    case 11:
+      res.innerText = `${monthOrder} month it's Autumn`;
+    default:
+      res.innerText = 'Incorrect value';
+  }
+  document.getElementById('month-input').value = '';
+}
+// 17. Створити розмітку  з полем введення, що приймає номер місяця та кнопкою. При натисканні на кнопку виводити кількість днів у цьому місяці.Використовувати switch.
 
-// Створити розмітку  з двома полями введення, що приймають числа та список (select) з варіантами вибору операцій: "+", "-", "*", "/". При натисканні на кнопку виводити результат обраної операції над цими числами. Користувач повинен бути попереджений про можливість ділення на нуль. Використовувати switch.
+function getQuantityDays() {
+  const monthOrder = Number(document.getElementById('month-days-input').value);
+  const resQuantityDays = document.getElementById('quantity-days-res');
 
-// Створити скрипт, який виводить на екран всі числа , які менші за n. Якщо зустрічається число, що більше або дорівнює n, цикл повинен бути закінчений за допомогою break.
+  switch (monthOrder) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      resQuantityDays.innerText = `In ${monthOrder} month 31 days`;
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      resQuantityDays.innerText = `In ${monthOrder} month 30 days`;
+      break;
+    case 2:
+      resQuantityDays.innerText = `In ${monthOrder} month 28 days`;
+      break;
+    default:
+      resQuantityDays.innerText = `Incorrectly value`;
+  }
+  document.getElementById('month-days-input').value = '';
+}
+
+// 18. Створити розмітку  з полем введення, що приймає назву кольору (наприклад, "червоний", "синій" і т.д.) та кнопкою. При натисканні на кнопку виводити відповідне повідомлення про дію: якщо це "червоний" - "стоп", якщо "зелений" - "йти", якщо "жовтий" - "чекати". Використовувати switch.
+
+function getColorRes() {
+  const color = document.getElementById('input-color').value.toLowerCase();
+  const colorRes = document.getElementById('color-res');
+
+  switch (color) {
+    case 'червоний':
+      colorRes.innerText = 'стоп';
+      break;
+    case 'зелений':
+      colorRes.innerText = 'йти';
+      break;
+    case 'жовтий':
+      colorRes.innerText = 'чекати';
+      break;
+    default:
+      colorRes.innerText = 'Немає такого кольору';
+  }
+  document.getElementById('input-color').value = '';
+}
+
+// 19. Створити розмітку  з двома полями введення, що приймають числа та список (select) з варіантами вибору операцій: "+", "-", "*", "/". При натисканні на кнопку виводити результат обраної операції над цими числами. Користувач повинен бути попереджений про можливість ділення на нуль. Використовувати switch.
+
+function getCalcRes() {
+  let firstNum = document.getElementById('input-number-first').value;
+  let secondNum = document.getElementById('input-number-second').value;
+  const operation = document.getElementById('calc-operation').value;
+  const res = document.getElementById('calc-res');
+
+  if (firstNum === '' && secondNum === '') {
+    res.innerText = 'Введіть значення';
+    return;
+  } else if (firstNum === '') {
+    res.innerText = 'Введіть перше значення';
+    return;
+  } else if (secondNum === '') {
+    res.innerText = 'Введіть друге значення';
+    return;
+  }
+
+  firstNum = Number(firstNum);
+  secondNum = Number(secondNum);
+  if (secondNum === 0 && operation === '/') {
+    res.innerText = 'На 0 ділити не можна';
+    return;
+  }
+  let result;
+  switch (operation) {
+    case '+':
+      result = sum(firstNum, secondNum);
+      break;
+    case '-':
+      result = sub(firstNum, secondNum);
+      break;
+    case '*':
+      result = mult(firstNum, secondNum);
+      break;
+    case '/':
+      result = div(firstNum, secondNum);
+      break;
+  }
+  return (res.innerText = `Результат: ${result}`);
+}
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+function sub(num1, num2) {
+  return num1 - num2;
+}
+function mult(num1, num2) {
+  return num1 * num2;
+}
+function div(num1, num2) {
+  return num1 / num2;
+}
+// 20. Створити скрипт, який виводить на екран всі числа , які менші за n. Якщо зустрічається число, що більше або дорівнює n, цикл повинен бути закінчений за допомогою break.
 const start = 1;
 const end = 50;
 const max = 30;
 for (let i = start; i <= end; i++) {
-    if(i>=max)break
-    console.log(i);
-    
+  if (i >= max) break;
+  // console.log(i);
 }
 
-// За допомогою циклу while вивести на екран всі числа від 1 до 20, крім чисел, кратних 3. Якщо зустрінете число, кратне 3, цикл повинен продовжити виконання за допомогою оператора continue.
+// 21. За допомогою циклу while вивести на екран всі числа від 1 до 20, крім чисел, кратних 3. Якщо зустрінете число, кратне 3, цикл повинен продовжити виконання за допомогою оператора continue.
 
 let i = 1;
 while (i <= 20) {
-    if (i % 3 === 0) {
-        i++
-        continue
-    };
-    console.log(i);
-    i++
+  if (i % 3 === 0) {
+    i++;
+    continue;
+  }
+  // console.log(i);
+  i++;
 }
